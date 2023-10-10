7ec8b374b1785c45e2aa16516091e486fa389074
Bernd Dammer <Bernd.Dammer@web.de>
2023-10-10 11:12:24 +0200

    first

---
+++ add_freertos.cmd
@@ -0,0 +1,8 @@
+rem create download and insert a new git
+rem call once this command to add the submodule to the project
+
+rem call ..\..\SetEnv.bat
+rem git submodule add https://github.com/FreeRTOS/FreeRTOS-Kernel freertos
+pause
+
+
---
+++ git_commit_feierabend.sh
@@ -0,0 +1,6 @@
+git add .
+git commit -m "first"
+git push
+git push --tags
+read
+
